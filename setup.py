0de754d2d392409e9215fa3d10f66243824fc41e
Dmitriy Vshivtsev <d.vshivtsev@vk.team>
2023-08-30 12:53:59 +0300

    refactor: new description and versions

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
     name='pageo',
     version=__version__,
     packages=['pageo'],
-    description='Утилита для расширенного взаимодействия с любым веб-сайтом и его страницами',
+    description='Библиотека на базе Selenium с расширенным функционалом. Предназначена для работы с любым веб-сайтом и его страницами',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='dmitry',
     author_email='dmitryvshivtsev@gmail.com',
     install_requires=[
-        'pytest==7.4.0',
-        'selenium==4.10.0',
+        'pytest>=7.4.0',
+        'selenium>=4.10.0',
     ],
 )
 
