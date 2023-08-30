9d34b2df971ddc437b4e1c28ad676437382542bf
Dmitriy Vshivtsev <d.vshivtsev@vk.team>
2023-08-30 08:05:58 +0300

    refactor: rename method go_to_site() to open_page()

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -3,12 +3,12 @@ from tests.test_page import TestPage
 
 def test_caption(driver, url):
     page = TestPage.with_driver(driver=driver, base_url=url)
-    page.go_to_site()
+    page.open_page()
     page.check_caption()
 
 
 def test_paragraphs(driver, url):
     page = TestPage.with_driver(driver=driver, base_url=url)
-    page.go_to_site()
+    page.open_page()
     page.check_paragraphs()
 
