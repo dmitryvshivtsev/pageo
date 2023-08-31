95efcf15bf41daa92bdea34207fe7ff1001b6f91
dmitryvshivtsev <dmitryvshivtsev@gmail.com>
2023-08-31 16:52:57 +0300

    fix packages + update version

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 setup(
     name='pageo',
     version=__version__,
-    packages=find_packages('pageo'),
-    package_dir={"": "pageo"},
+    packages=find_packages(exclude=['tests']),
     description='Библиотека на базе Selenium с расширенным функционалом. Предназначена для работы с любым веб-сайтом и его страницами',
     long_description=readme,
     long_description_content_type='text/markdown',
