998eb348e88d14d756158c561ccc70a8d17a9c29
dmitryvshivtsev <d.vshivtsev@vk.team>
2023-09-05 13:51:06 +0300

    add tests for fixtures

---
+++ tests/test_conftest.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+
+def test_fixture_driver(driver):
+    assert isinstance(driver, webdriver.Chrome)
+
+
+def test_fixture_options(chrome_options):
+    assert isinstance(chrome_options, webdriver.ChromeOptions)
+
+
+def test_fixture_url(url):
+    assert url == 'http://127.0.0.1:8000'
