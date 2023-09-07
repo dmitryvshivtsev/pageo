fd831ba34a3c88ca511a96e78044ca85835b00c5
dmitryvshivtsev <d.vshivtsev@vk.team>
2023-09-07 12:41:17 +0300

    build: new version & add classifiers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 setup(
     name='pageo',
@@ -18,6 +18,17 @@ setup(
     install_requires=[
         'pytest>=7.4.0',
         'selenium>=4.10.0',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
     ],
 )
 
