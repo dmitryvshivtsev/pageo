f0216422d45ca87139b6fa4217522760f0890714
dmitryvshivtsev <d.vshivtsev@vk.team>
2023-09-07 12:34:39 +0300

    refactor: a little refactoring

--- pageo/locators/abstract_locator.py
+++ pageo/locators/abstract_locator.py
@@ -12,23 +12,22 @@ class AbstractLocator(ABC):
     Пример использования наследника в связке с BasePage:
     >>> from pageo.base_page import BasePage
     >>> from pageo.locators.id_locator import IdLocator
-
-
+    >>>
+    >>>
     >>> class AboutPage(BasePage):
-    >>>     search_field_element = IdLocator("id-search-field")
-
-    >>>     def is_search_field(self):
-    >>>         return True if self.search_field_element else False
+    ...     search_field_element = IdLocator("id-search-field")
+    ...     def is_search_field(self):
+    ...         return True if self.search_field_element else False
     """
-    def __init__(self, selector: str, is_many: bool = False, duration: int = 5):
+    def __init__(self, selector: str, is_many: bool = False, timeout: int = 5):
         self.selector = selector
         self.is_many = is_many
-        self.duration = duration
+        self.timeout = timeout
 
     def __get__(self, instance, owner=None):
         if self.is_many:
-            return instance.find_elements(self.by, self.selector, self.duration)
-        return instance.find_element(self.by, self.selector, self.duration)
+            return instance.find_elements(self.by, self.selector, self.timeout)
+        return instance.find_element(self.by, self.selector, self.timeout)
 
     def set_name(self, name):
         self.name = name
