79c5959f210fc69ccab1a33bcbb8c425c3510e10
dmitryvshivtsev <dmitryvshivtsev@gmail.com>
2023-12-11 17:21:28 +0300

    build: fix package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 setup(
     name='pageo',
